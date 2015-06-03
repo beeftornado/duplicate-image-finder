5c3b44a91558dd602681fe459427561b692739f0
Casey Duquette <casey.duquette@gmail.com>
2015-06-02 22:16:47 -0700

    Support json output format

--- duplicateimagefinder/output_formats/__init__.py
+++ duplicateimagefinder/output_formats/__init__.py
@@ -1,8 +1,11 @@
 from duplicateimagefinder.enums import *
 import human
+import jsono
 
 
 def outputter_for_format(fmt):
     if fmt is Formats.HUMAN_READABLE:
         return human.HumanFormat()
+    if fmt is Formats.JSON:
+        return jsono.JsonFormat()
     else: return human.HumanFormat()
--- duplicateimagefinder/output_formats/jsono.py
+++ duplicateimagefinder/output_formats/jsono.py
@@ -1,17 +1,19 @@
 from types import *
+import json
 import sys
 
 import base
 
 
-class HumanFormat(base.BaseFormatter):
+class JsonFormat(base.BaseFormatter):
     def output(self, data):
         assert type(data) is ListType, "data is not a list"
 
         # List the images that are similar to each other
+        sys.stdout.old_write('[')
         for similar in data:
             assert isinstance(similar, base.OutputRecord), "record is not instance of OutputRecord"
-            sys.stdout.old_write("%s is %d%% similar to %s\n" % (
-                similar.image1, similar.similarity_pct, similar.image2
-            ))
+            sys.stdout.old_write(json.dumps({'image1': similar.image1, 'image2': similar.image2,
+                              'similarity': similar.similarity_pct}) + ',')
             sys.stdout.flush()
+        sys.stdout.old_write(']')
